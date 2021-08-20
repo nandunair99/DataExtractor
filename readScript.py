5df0c4e05ba43757091361acaf77fd03f3d8d417
nandakrishnan nair <nandu.99.nair@gmail.com>
2021-08-20 23:27:13 +0530

    format data and insert into database country wise

--- readScript.py
+++ readScript.py
@@ -1,12 +1,12 @@
 import mysql.connector as msc
 import pandas as pd
-
+import sqlalchemy
 
 def fetch_data():
     df = pd.read_csv("sample.txt", skiprows=1, delimiter="|",
-                     names=["DataType", "Customer Name", "Customer ID", "Customer Open Date", "Last Consulted Date",
-                            "Vaccination Type", "Doctor Consulted", "State", "Country", "Post Code", "Date of Birth",
-                            "Active Customer"])
+                     names=["DataType", "CustomerName", "CustomerID", "CustomerOpenDate", "LastConsultedDate",
+                            "VaccinationType", "DoctorConsulted", "State", "Country", "PostCode", "DateofBirth",
+                            "ActiveCustomer"])
     df=df[df['DataType'] == 'D']
     print(df.dtypes)
     for i in range(len(df)):
@@ -14,50 +14,73 @@ def fetch_data():
 
     return df
 
+def format_date(i,df):
+    x = df.iloc[i, 10]
+    x=str(x)
+    l=len(x)
+    if(l==7):
+        #print(7)
+        year = x[3:7]
+        month = x[1:3]
+        date = '0'+x[:1]
+    else:
+        #print(8)
+        year = x[4:8]
+        month = x[2:4]
+        date = x[:2]
+
+
+    date=year+month+date
+    return date
+
 def insert_data(df):
+    mydb = msc.connect(host="localhost", user="root", password="1970", database="incubyte")
+    mycursor = mydb.cursor()
+
     try:
-        mydb = msc.connect(host="localhost", user="root", password="1970", database="incubyte")
-        mycursor = mydb.cursor()
+        for i in range(len(df)):
 
-        CreateQuery = """CREATE TABLE IF NOT EXISTS Customer( 
-                                 CustomerName VARCHAR(255) NOT NULL primary key,
-                                 CustomerID VARCHAR(18) NOT NULL,
-                                 CustomerOpenDate DATE NOT NULL,
-                                 LastConsultedDate DATE,
-                                 VaccinationType CHAR(5),
-                                 DoctorConsulted CHAR(255),
-                                 State CHAR (5),
-                                 Country CHAR (5),
-                                 PostCode INT(5),
-                                 DateofBirth DATE,
-                                 ActiveCustomer CHAR(1)
-                                 ) """
+            CreateQuery = """CREATE TABLE IF NOT EXISTS """+df.iloc[i, 8]+"""(
+                                     CustomerName VARCHAR(255) NOT NULL primary key,
+                                     CustomerID VARCHAR(18) NOT NULL,
+                                     CustomerOpenDate DATE NOT NULL,
+                                     LastConsultedDate DATE,
+                                     VaccinationType CHAR(5),
+                                     DoctorConsulted CHAR(255),
+                                     State CHAR (5),
+                                     Country CHAR (5),
+                                     PostCode INT(5),
+                                     DateofBirth DATE,
+                                     ActiveCustomer CHAR(1)
+                                     ) """
 
-        mycursor.execute(CreateQuery)
-        print("Customer Table created successfully ")
-        InsertQuery="""
-        INSERT INTO Customer (CustomerName, CustomerID, CustomerOpenDate,LastConsultedDate, VaccinationType, DoctorConsulted,State, Country, PostCode,DateofBirth, ActiveCustomer)
-         VALUES ("%df.iloc[0, 1]", %df.iloc[0, 2], %df.iloc[0, 3], %df.iloc[0, 4], "%df.iloc[0, 5]", "%df.iloc[0, 6]", "%df.iloc[0, 7]", "%df.iloc[0, 8]", %df.iloc[0, 9], df.iloc[0, 10], "%df.iloc[0, 10]")
-         WHERE NOT EXISTS (
-         SELECT name FROM CustomerName WHERE CustomerID='%df.iloc[0, 2]'
-         )
-        """
-        mycursor.execute(InsertQuery)
-        print("Customer Inserted successfully ")
+            mycursor.execute(CreateQuery)
+            print("Table created successfully ")
+            InsertQuery = """INSERT INTO """+df.iloc[i, 8]+""" (CustomerName, CustomerID, CustomerOpenDate,LastConsultedDate, VaccinationType, DoctorConsulted,State, Country, PostCode,DateofBirth, ActiveCustomer) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) """
+            print(df)
+
+
+            mycursor.execute(InsertQuery, (
+            df.iloc[i, 1], int(df.iloc[i, 2]), int(df.iloc[i, 3]), int(df.iloc[i, 4]), df.iloc[i, 5], df.iloc[i, 6],
+            df.iloc[i, 7], df.iloc[i, 8], int(df.iloc[i, 9]), int(format_date(i,df)), df.iloc[i, 11]))
+
+            print("Customer Inserted successfully ")
 
     except msc.Error as error:
-        print("Failed to create table in MySQL: {}".format(error))
+            print("Failed to create table in MySQL: {}".format(error))
     finally:
-        if mydb.is_connected():
-            mycursor.close()
-            mydb.close()
-            print("MySQL connection is closed")
-    print(df)
+            if mydb.is_connected():
+                mydb.commit()
+                mycursor.close()
+                mydb.close()
+                print("MySQL connection is closed")
+
 
 
 def main():
     print("hello in ")
     df=fetch_data()
     insert_data(df)
+
 if __name__=="__main__":
     main()
