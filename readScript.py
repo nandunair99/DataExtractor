41d414b34225b096754621c33252ed5102ed5ace
nandakrishnan nair <nandu.99.nair@gmail.com>
2021-08-21 23:34:54 +0530

    Handling test cases by passing null values for non-mandatory fields

--- readScript.py
+++ readScript.py
@@ -9,12 +9,22 @@ def fetch_data():
                             "VaccinationType", "DoctorConsulted", "State", "Country", "PostCode", "DateofBirth",
                             "ActiveCustomer"])
 
+
     df=df.loc[df['DataType'] == 'D']
     df=df.drop(['Spacing'], axis=1)
 
-    #typecasting for compatibility with mysql
-    df[["LastConsultedDate","PostCode","DateofBirth"]]=df[["LastConsultedDate","PostCode","DateofBirth"]].astype(int)
-    print(df.dtypes)
+    # handling Null data
+    df["LastConsultedDate"].fillna("null", inplace = True)
+    df["VaccinationType"].fillna("null", inplace=True)
+    df["DoctorConsulted"].fillna("null", inplace=True)
+    df["State"].fillna("null", inplace=True)
+    df["PostCode"].fillna("null", inplace=True)
+    df["DateofBirth"].fillna(0, inplace=True)
+    df["ActiveCustomer"].fillna("-", inplace=True)
+    df["Country"].fillna("Other", inplace=True) #country not mentioned then default set to "other"
+   
+
+
 
 
     return df
@@ -22,22 +32,27 @@ def fetch_data():
 #formating date of birth column values from dd/mm/yyyy to yyyy/mm/dd format
 def format_date(i,df):
     x = df.iloc[i, 10]
-    x=str(x)
-    l=len(x)
-    if(l==7):
-        #print(7)
-        year = x[3:7]
-        month = x[1:3]
-        date = '0'+x[:1]
+    if(x!="null"):
+        x = int(x)
+        x = str(x)
+        l = len(x)
+        print(l)
+        if (l == 7):
+            year = x[3:7]
+            month = x[1:3]
+            date = '0' + x[:1]
+        else:
+            year = x[4:8]
+            month = x[2:4]
+            date = x[:2]
+
+        date = year + month + date
+
+        return int(date)
     else:
-        #print(8)
-        year = x[4:8]
-        month = x[2:4]
-        date = x[:2]
+        return 0
 
 
-    date=year+month+date
-    return date
 
 
 
@@ -49,7 +64,7 @@ def insert_data(df):
 
     try:
         for i in range(len(df)):
-            #Creating table if does not exist with table name as country
+            #Creating table if does not exist with table name as country i.e df.iloc[i, 8]
             CreateQuery = """CREATE TABLE IF NOT EXISTS """+df.iloc[i, 8]+"""(
                                      CustomerName VARCHAR(255) NOT NULL primary key,
                                      CustomerID VARCHAR(18) NOT NULL,
@@ -67,14 +82,15 @@ def insert_data(df):
             mycursor.execute(CreateQuery)
             print("Table created successfully ")
 
+
             #inserting into database tables
             InsertQuery = """INSERT INTO """+df.iloc[i, 8]+""" (CustomerName, CustomerID, CustomerOpenDate,LastConsultedDate, VaccinationType, DoctorConsulted,State, Country, PostCode,DateofBirth, ActiveCustomer) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) """
             print(df)
-#handling Integrity constraints
+            #handling Integrity constraints
             try:
                 mycursor.execute(InsertQuery, (
-                df.iloc[i, 1], int(df.iloc[i, 2]), int(df.iloc[i, 3]), int(df.iloc[i, 4]), df.iloc[i, 5],
-                df.iloc[i, 6],df.iloc[i, 7], df.iloc[i, 8], int(df.iloc[i, 9]), int(format_date(i, df)), df.iloc[i, 11]))
+                df.iloc[i, 1], int(df.iloc[i, 2]), int(df.iloc[i, 3]), 0 if df.iloc[i, 4]=="null" else int(df.iloc[i, 4]), df.iloc[i, 5],
+                df.iloc[i, 6],df.iloc[i, 7], df.iloc[i, 8], 0 if df.iloc[i, 9]=="null" else int(df.iloc[i, 9]), (format_date(i, df)), df.iloc[i, 11]))
 
                 print("Customer Inserted successfully ")
             except mysql.connector.IntegrityError as exc:
@@ -83,6 +99,7 @@ def insert_data(df):
 
 
     except msc.Error as error:
+        #create table exception
             print("Failed to create table in MySQL: {}".format(error))
     finally:
             if mydb.is_connected():
@@ -94,7 +111,7 @@ def insert_data(df):
 
 
 def main():
-    print("hello in ")
+
     df=fetch_data()
     insert_data(df)
 
